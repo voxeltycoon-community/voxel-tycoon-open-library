20b6e9b543eb0a2769062c259f97bb1b369ec5f6
evertn <evert_noorlander@hotmail.com>
2021-11-23 19:02:22 +0100

    Create VTOLReflectionHelpers.cs
    - Added a new class VTOLReflectionHelpers
    - Added a method to VTOLReflectionHelpers

---
+++ Voxel Tycoon Open Library/VTOLReflectionHelpers.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Reflection;
+
+namespace VTOL
+{
+    /// <summary>
+    /// Class with methods to help with reflection 
+    /// </summary>
+    public static class VTOLReflectionHelpers
+    {
+        /// <summary>
+        /// Allows for setting a property which has a private set accessor.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="obj">The object holding the property.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="value">The value the property should be set to.</param>
+        public static void SetReadOnlyProperty<T>([NotNull] this object obj, [NotNull] string propertyName, T value)
+        {
+            PropertyInfo property = obj.GetType().GetProperty(propertyName);
+
+            if (property == null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            if (property.PropertyType.IsAssignableFrom(typeof(T)))
+            {
+                throw new ArgumentException($"{value} is not of type {property.PropertyType}.");
+            }
+
+            property = property.DeclaringType.GetProperty(propertyName);
+            property.GetSetMethod(true).Invoke(obj, new object[] { value });
+        }
+    }
+}
