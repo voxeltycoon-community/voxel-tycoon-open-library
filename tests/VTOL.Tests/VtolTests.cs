627cdb8aa1ae8a962eb18e938f909d87f48198e8
evertn <evert_noorlander@hotmail.com>
2022-02-02 19:30:30 +0100

    minor Sorting fix and minor updates
    - Adjusted .editorconfig
    - Fixed sorting of the ConnectionFilters so it only happens once
    - Replaced ISet with List
    - Several naming updates (Listener to ConnectionFilter)
    - Added the source building as a propety to PotentialConnectionArgs
    - Updated some documentation

---
+++ tests/VTOL.Tests/VtolTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+using NUnit.Framework;
+
+namespace VTOL
+{
+	class VtolTests
+	{
+		[Test]
+		public void ExecuteOnGameStarting()
+		{
+			Vtol vtol = new Vtol();
+
+			MethodInfo method = typeof(Vtol).GetMethod("OnGameStarting", BindingFlags.NonPublic | BindingFlags.Instance);
+			Assert.NotNull(method);
+			method.Invoke(vtol, Array.Empty<object>());
+		}
+	}
+}
