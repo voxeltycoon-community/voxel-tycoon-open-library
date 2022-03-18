f9e360027d7da97243950c07853969bd5cf6b78d
evertn <evert_noorlander@hotmail.com>
2022-03-18 15:25:29 +0100

    RegisterConnectionFilter-method update
    Changed the RegisterConnectionFilter-method so it is no longer required to register a IConnectionFilter with RegisterConnectionFilter(new IConnectionFilter, priority). Instead you can just give the type of the class you want to register.
    
    - Changed the original RegisterConnectionFilter() to private
    - Removed summary from original RegisterConnectionFilter-method
    - Added public RegisterConnectionFilter<TFilter>(double priority = 0)
    - Added summary to new RegisterConnectionFilter-method

--- Voxel Tycoon Open Library/StorageNetwork/ConnectionManager/InvalidateSiblingsPatch.cs
+++ Voxel Tycoon Open Library/StorageNetwork/ConnectionManager/InvalidateSiblingsPatch.cs
@@ -8,7 +8,7 @@ namespace VTOL.StorageNetwork.ConnectionManager
 	/// This class is used to control which <see cref="StorageNetworkBuilding"/> can be connected with eachother.
 	/// Voxel Tycoon only dictates that certain buildings can connect with eachother based on their type. With this patch a system is introduced where it is also possible to filter connections based on custom logic.
 	/// Filters are classes implementing <see cref="IConnectionFilter"/>, and have the functionality to allow or disallow a connection between two <see cref="StorageNetworkBuilding"/>. 
-	/// These filter classes are made by the user and can be registered with <see cref="ConnectionController.RegisterConnectionFilter(IConnectionFilter, double)"/>.
+	/// These filter classes are made by the user and can be registered with <see cref="ConnectionController.RegisterConnectionFilter{TFilter}(double)"/>.
 	/// Every time a new <see cref="StorageNetworkBuilding"/> is placed, Voxel Tycoon will update the Storage Network, but only for the <see cref="StorageNetworkBuilding"/> which are in range of the placed building.
 	/// To update the Storage Network, Voxel Tycoon will use <see cref="StorageBuildingManager.FindSiblings(StorageNetworkBuilding)"/> for each <see cref="StorageNetworkBuilding"/> that needs an update and returns a list with all potential connections.
 	/// Before this list with potential connections is returned, <see cref="InvalidateSiblingsPatch"/> allows all filters to cycle through this list and decide if a connection should be allowed or not.
