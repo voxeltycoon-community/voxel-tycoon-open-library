a0362bd5570089ba16192b2130d11ddaf7028e65
evertn <evert_noorlander@hotmail.com>
2022-05-02 14:23:52 +0200

    Summary corrections

--- Voxel Tycoon Open Library/StorageNetwork/ConnectionManager/MarkBuildingDirtyPatch.cs
+++ Voxel Tycoon Open Library/StorageNetwork/ConnectionManager/MarkBuildingDirtyPatch.cs
@@ -6,7 +6,8 @@ namespace VTOL.StorageNetwork.ConnectionManager
 	/// <summary>
 	/// This patch makes sure that a <see cref="StorageNetworkBuilding"/> updates it's Storage Network in the same frame it has been built.
 	/// <para>After some investigating we found that a <see cref="StorageNetworkBuilding"/> is not updating it's Storage Network in the frame it has been build. Instead it would only update whenever another <see cref="StorageNetworkBuilding"/> is build within range.
-	/// For the Connection Filters to update immediately it is necessary that the network is updated in the same frame the building built. This patch makes sure the built building is marked as dirty, so the Storage Network is updating its connections.</para>
+	/// For the Connection Filters to update immediately it is necessary that the network is updated in the same frame the building is built. This patch makes sure the built building is marked as dirty, 
+	/// so the Storage Network is updating its connections in the same frame.</para>
 	/// </summary>
 	[HarmonyPatch(typeof(StorageNetworkBuilding))]
 	[HarmonyPatch("OnBuilt")]
